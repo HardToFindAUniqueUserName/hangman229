f6c49132bae34ce6df5dbb240293b19ffa0ee711
HardToFindAUniqueUserName <paul.mayer@charitycommission.gov.uk>
2023-12-01 13:50:20 +0000

    Final docstring amendment

--- milestone_5.py
+++ milestone_5.py
@@ -1,5 +1,11 @@
 import random
-
+"""
+This module recreates the classic Hangeman game, in code.
+Hangman is a game in which a player thinks of a word and 
+the other player tries to guess that word within a certain amount of attempts. 
+This is an implementation of the Hangman game, 
+where the computer thinks of a word and the user tries to guess it.
+"""
 class Hangman:
     def __init__(self, word_list, number_of_lives = 5):
         """Parameters:"""
